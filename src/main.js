61e67dc09c328b7bba892d510992a850f567a1d9
tanacchi <q111026d@mail.kyutech.jp>
2020-05-06 19:59:57 +0900

    Import firebase config from another file

--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
 import firebase from 'firebase'
 import Vue from 'vue'
 import App from './App.vue'
+import {firebaseConfig} from './../firebase_config.js'
 
 Vue.config.productionTip = false
 
-var firebaseConfig = {
-  apiKey: "",
-  authDomain: "lt-hub.firebaseapp.com",
-  databaseURL: "https://lt-hub.firebaseio.com",
-  projectId: "lt-hub",
-  storageBucket: "lt-hub.appspot.com",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-}
 firebase.initializeApp(firebaseConfig)
 
 new Vue({

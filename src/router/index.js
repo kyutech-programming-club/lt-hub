0b049149e3c89d291564791874a5cbce7ca40064
tanacchi <q111026d@mail.kyutech.jp>
2020-05-09 17:51:38 +0900

    Add events page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Events from '../views/Events.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    component: Events
   },
 ]
 
---
+++ src/views/Events.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>This is Events page</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+<style>
+</style>

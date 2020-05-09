dc9733dc65319c67fd30752ef580d62be9fd0068
tanacchi <q111026d@mail.kyutech.jp>
2020-05-09 17:11:18 +0900

    Create views directory

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <github-auth msg="Welcome to LT-Hub"/>
+  </div>
+</template>
+
+<script>
+import GithubAuth from './components/GithubAuth.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GithubAuth
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

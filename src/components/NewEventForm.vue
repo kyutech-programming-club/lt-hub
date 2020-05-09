243d3b487f0919264771eccfef711c58489a1fd4
tanacchi <q111026d@mail.kyutech.jp>
2020-05-09 20:34:25 +0900

    Add watch functions to check form variables

--- src/components/NewEventForm.vue
+++ src/components/NewEventForm.vue
@@ -10,17 +10,22 @@
       </v-card-title>
       <v-card-text>
         <v-text-field
-          v-model="newEventTitle"
+          v-model="title"
           label="イベント名" />
         <v-text-field
+          v-model="description"
           label="イベント概要" />
         <v-text-field
+          v-model="author"
           label="作成者" />
         <v-date-picker
+          v-model="startDate"
           label="開始日"/>
         <v-time-picker
+          v-model="startTime"
           label="開始時刻"/>
         <v-text-field
+          v-model="place"
           label="会場" />
       </v-card-text>
     </v-card>
@@ -30,8 +35,33 @@
 export default {
   data: function() {
     return {
-      newEventTitle: "",
+      title: "",
+      description: "",
+      author: "",
+      startDate: "",
+      startTime: "",
+      place: "",
     };
+  },
+  watch: {
+    title: function() {
+      console.log("title: "+this.title);
+    },
+    description: function() {
+      console.log("description: "+this.description);
+    },
+    author: function() {
+      console.log("author: "+this.author)
+    },
+    startDate: function() {
+      console.log("startDate: "+this.startDate);
+    },
+    startTime: function() {
+      console.log("startTime: "+this.startTime);
+    },
+    place: function() {
+      console.log("place: "+this.place);
+    },
   },
 };
 </script>

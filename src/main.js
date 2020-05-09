5d3f5bf2d055c185f2b60d8568db9226b985b41e
tanacchi <q111026d@mail.kyutech.jp>
2020-05-09 19:31:29 +0900

    Install and use vuetify package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
 import firebase from 'firebase'
 import {firebaseConfig} from './../firebase_config.js'
 
@@ -10,5 +11,6 @@ firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App),
 }).$mount('#app')

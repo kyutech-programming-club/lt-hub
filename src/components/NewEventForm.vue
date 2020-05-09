2e64eda9eeaf83339c945d921041bc0399634924
tanacchi <q111026d@mail.kyutech.jp>
2020-05-09 20:00:29 +0900

    Add elements of form for new event

--- src/components/NewEventForm.vue
+++ src/components/NewEventForm.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="new-item-form">
-    <p>{{new_event_title}}</p>
-    <input v-model="new_event_title" />
+    <v-card class="pa-4 ma-6">
+      <v-card-title>
+        <v-toolbar>
+          <v-toolbar-title class=mx-auto>
+            イベント新規作成
+          </v-toolbar-title>
+        </v-toolbar>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field
+          v-model="newEventTitle"
+          label="イベント名" />
+        <v-text-field
+          label="イベント概要" />
+        <v-text-field
+          label="作成者" />
+        <v-date-picker
+          label="開始日"/>
+        <v-time-picker
+          label="開始時刻"/>
+        <v-text-field
+          label="会場" />
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 <script>
 export default {
   data: function() {
     return {
-      new_event_title: "",
+      newEventTitle: "",
     };
   },
 };

8c507f1fba27db3ab18d79b2b296d9a249f7bf92
tanacchi <q111026d@mail.kyutech.jp>
2020-05-09 17:26:05 +0900

    Remake App.vue and main.js for vue-router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import firebase from 'firebase'
 import {firebaseConfig} from './../firebase_config.js'
 
 Vue.config.productionTip = false
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 firebase.initializeApp(firebaseConfig)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')

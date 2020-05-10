84b839839fc69a41bc2ec811aaaa6b71c72466b3
tanacchi <q111026d@mail.kyutech.jp>
2020-05-10 16:28:45 +0900

    Use expansion-panel for new event form

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="events">
     <h1>This is Events page</h1>
-    <new-event-form />
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header>Open</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <new-event-form />
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 

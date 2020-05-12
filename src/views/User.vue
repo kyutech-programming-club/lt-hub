1127f7d158334d98f029348791b53bc892dac1eb
ryuto <rryuto5210@gmail.com>
2020-05-12 22:52:31 +0900

    Update user page | Add current user

--- src/views/User.vue
+++ src/views/User.vue
@@ -5,6 +5,9 @@
       <img :src="user.photoURL"/><br>
       [{{ user.name }}]<br>
     </div>
+    <div v-if="current">
+      <b>This is My Page</b>
+    </div>
 
   </div>
 </template>
@@ -18,42 +21,34 @@
     data() {
       return {
         user: {},
+        current: false
       }
     },
     created() {
       let self = this
-      console.log("User Page");
-      db.collection("users")
+      console.log('User Page');
+      db.collection('users')
         .doc(this.$route.params['uid'])
         .get()
         .then(function(dbUser) {
           if (dbUser.exists) {
-            console.log('Successfully fetched user data:', dbUser.data());
+            console.log('Successfully fetched user data');
+            // console.log(JSON.stringify(dbUser.data()));
             self.user = dbUser.data();
           } else {
-            firebase.auth().onAuthStateChanged(currentUser => {
-              console.log("Creating user data...");
-              db.collection('users')
-                .doc(currentUser.uid)
-                .set({
-                  name: currentUser.displayName,
-                  photoURL: currentUser.photoURL
-                })
-                .then(() => {
-                  console.log('Successfully created new user:', currentUser.displayName);
-                  self.$router.go(self.$router.currentRoute)
-                })
-                .catch(err => {
-                  console.error('Error Creating new user: ', err);
-                });
-            });
+            console.error('Error fetching user data');
+            self.user = {};
           }
         })
         .catch(err => {
           console.error('Error fetching user data: ', err);
         });
-    },
-    methods: {
+
+      firebase.auth().onAuthStateChanged(user => {
+        if (user != null && user.uid == this.$route.params['uid']) {
+          self.current = true;
+        }
+      })
     }
   }
 </script>

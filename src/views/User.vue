78b7a19c9f32cc16111e4a4b70367fef8f59077e
ryuto <rryuto5210@gmail.com>
2020-05-13 23:55:48 +0900

    Add user name update

--- src/views/User.vue
+++ src/views/User.vue
@@ -7,6 +7,34 @@
     </div>
     <div v-if="current">
       <b>This is My Page</b>
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <v-card-title>
+              <v-toolbar :flat="true">
+                <v-toolbar-title class="mx-autoi">
+                  Edit
+                </v-toolbar-title>
+              </v-toolbar>
+            </v-card-title>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card class="pa-4 ma-6">
+              <v-card-text>
+                <v-text-field
+                  v-model="name"
+                  label="Name" />
+                <v-btn
+                  color="blue"
+                  :x-large="true"
+                  @click="update">
+                  更新
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
 
   </div>
@@ -21,7 +49,8 @@
     data() {
       return {
         user: {},
-        current: false
+        current: false,
+        name: ""
       }
     },
     created() {
@@ -35,6 +64,7 @@
             console.log('Successfully fetched user data');
             // console.log(JSON.stringify(dbUser.data()));
             self.user = dbUser.data();
+            self.name = dbUser.data().name;
           } else {
             console.error('Error fetching user data');
             self.user = {};
@@ -49,6 +79,28 @@
           self.current = true;
         }
       })
+    },
+    watch: {
+      name: function() {
+        console.log("name: "+this.name);
+      }
+    },
+    methods: {
+      update: function() {
+        console.log('update...');
+        db.collection('users')
+          .doc(this.$route.params['uid'])
+          .update({
+            name: this.name
+          })
+          .then(() => {
+            console.log(`User ${this.name} was updated.`);
+            this.$router.go(this.$router.currentRoute);
+          })
+          .catch(err => {
+            console.error(`Error occurd in update: ${err}`);
+          });
+      }
     }
   }
 </script>

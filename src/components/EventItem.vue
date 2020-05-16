f6cf0bccd765aa97a8c868f8d1fd91642ea60796
ryuto <rryuto5210@gmail.com>
2020-05-16 23:10:03 +0900

    Update events list info

--- src/components/EventItem.vue
+++ src/components/EventItem.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="event-item">
-    {{ event.title }}
+    <v-card class="pa-4 ma-6">
+      <v-card-title>{{event.title}}</v-card-title>
+      <v-card-text>
+        期間：{{event.startDate}} {{event.startTime}} ~ {{event.endDate}} {{event.endTime}}<br>
+        責任者：{{event.author}}
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 

14df937136906d626fa89a8aae10e92ad9d861ee
ryuto <rryuto5210@gmail.com>
2020-06-05 22:41:02 +0900

    View event createTime updateTime

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -5,8 +5,8 @@
       <h2>{{ event.data.title }}</h2>
       期間：{{ event.data.start }} ~ {{ event.data.end }}<br>
       場所：{{ event.data.place }}<br>
-      作成日時：{{ event.data.createTime }}<br>
-      最終更新日時：{{ event.data.updateTime }}
+      作成日時：{{ getStringFromDate(event.data.createTime.toDate()) }}<br>
+      最終更新日時：{{ getStringFromDate(event.data.updateTime.toDate()) }}
     </div>
     <div v-if="author.id">
       責任者：
@@ -174,6 +174,32 @@
         } catch (err) {
           console.log(err);
         }
+      },
+      //日付から文字列に変換する関数
+      getStringFromDate(date) {
+        var year_str = date.getFullYear();
+        //月だけ+1すること
+        var month_str = 1 + date.getMonth();
+        var day_str = date.getDate();
+        var hour_str = date.getHours();
+        var minute_str = date.getMinutes();
+        var second_str = date.getSeconds();
+        
+        month_str = ('0' + month_str).slice(-2);
+        day_str = ('0' + day_str).slice(-2);
+        hour_str = ('0' + hour_str).slice(-2);
+        minute_str = ('0' + minute_str).slice(-2);
+        second_str = ('0' + second_str).slice(-2);
+        
+        var format_str = 'YYYY-MM-DD hh:mm:ss';
+        format_str = format_str.replace(/YYYY/g, year_str);
+        format_str = format_str.replace(/MM/g, month_str);
+        format_str = format_str.replace(/DD/g, day_str);
+        format_str = format_str.replace(/hh/g, hour_str);
+        format_str = format_str.replace(/mm/g, minute_str);
+        format_str = format_str.replace(/ss/g, second_str);
+        
+        return format_str;
       }
     }
   }

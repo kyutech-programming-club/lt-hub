2a7a4c86f9d4503201cfa52cf1001ceca74fe265
syu-kwsk <wataru1165kwsk@gmail.com>
2020-06-13 14:15:44 +0900

    Change comment form from button

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,39 +1,25 @@
 <template>
   <div>
-    <!--フォームの表示ボタン-->
-    <v-btn
-      color="blue"
-      dark
-      center
-      fab
-      fixed
-      right
-      @click="showCreateForm"
-    >
-      <v-icon>comment</v-icon>
-    </v-btn>
-    <v-dialog v-model="displayForm" max-width="500px">
-      <!--コメント入力フォーム-->
-      <v-card>
-        <v-container>
-          <h2>コメント追加</h2>
-          <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="inputComment"
-              :rules="commentRules"
-              label="コメント"
-              required
-            ></v-text-field>
-            <v-btn
-              :disabled="!valid"
-              @click="addComment"
-            >
-              投稿する
-            </v-btn>
-          </v-form>
-        </v-container>
-      </v-card>
-    </v-dialog>
+    <v-card>
+      <v-container>
+        <h2>コメント追加</h2>
+        <v-form ref="form" v-model="valid" lazy-validation @submit.prevent>
+          <v-text-field
+            v-model="inputComment"
+            :rules="commentRules"
+            label="コメント"
+            required
+          ></v-text-field>
+          <v-btn
+            :disabled="!valid"
+            @click="addComment"
+          >
+            投稿する
+          </v-btn>
+        </v-form>
+      </v-container>
+    </v-card>
+
   </div>
 </template>
 
@@ -72,22 +58,11 @@
           userRef: userRef,
           createdAt: now
         })
-        // ダイアログを閉じる
-        this.hideCreateForm()
+        this.clear();
       },
       // Formの初期化
       clear() {
         this.$refs.form.reset()
-      },
-      // Formダイアログの表示
-      showCreateForm() {
-        this.displayForm = true
-      },
-      //
-      // Formダイアログの非表示
-      hideCreateForm() {
-        this.clear()
-        this.displayForm = false
       },
     },
   }

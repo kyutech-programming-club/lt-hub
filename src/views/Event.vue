93215a1183f4aa00e073e46107971c58ce7e3390
syu-kwsk <wataru1165kwsk@gmail.com>
2020-06-14 18:30:51 +0900

    Show current user participate or not in realtime

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -12,7 +12,7 @@
         責任者：
         <user-item-small
           :user = "event.author" />
-        <div v-if="event.author.id==currentUserId">
+        <div v-if="event.author.id == currentUserId">
           <edit-event-form :event="event"/>
           <v-btn class="white--text font-weight-bold" color="#ff4b4b" @click="deleteEvent">
             Delete
@@ -20,38 +20,19 @@
         </div>
       </div>
     </div>
-
-    <!--    <div v-if="participated">-->
-    <!--      <v-btn class="white&#45;&#45;text font-weight-bold" color="#ff4b4b" @click="cancelParticipate">-->
-    <!--        参加取り消し-->
-    <!--      </v-btn>-->
-    <!--      <v-expansion-panels>-->
-    <!--        <v-expansion-panel>-->
-    <!--          <v-expansion-panel-header>-->
-    <!--            <v-card-title>-->
-    <!--              <v-toolbar :flat="true">-->
-    <!--                <v-toolbar-title class="mx-autoi">-->
-    <!--                  トーク新規作成-->
-    <!--                </v-toolbar-title>-->
-    <!--              </v-toolbar>-->
-    <!--            </v-card-title>-->
-    <!--          </v-expansion-panel-header>-->
-    <!--          <v-expansion-panel-content>-->
-    <!--            <new-talk-form-->
-    <!--              :eventId="event.id"/>-->
-    <!--          </v-expansion-panel-content>-->
-    <!--        </v-expansion-panel>-->
-    <!--      </v-expansion-panels>-->
-    <!--    </div>-->
-    <!--    <div v-else-if="currentUserId">-->
-    <!--      <v-btn-->
-    <!--        class="white&#45;&#45;text font-weight-bold"-->
-    <!--        color="#009eff"-->
-    <!--        @click="participate">-->
-    <!--        参加-->
-    <!--      </v-btn>-->
-    <!--    </div>-->
-
+    <div  v-if="isParticipated">
+      <v-btn class="white--text font-weight-bold" color="#ff4b4b" @click="cancelParticipate">
+        参加取り消し
+      </v-btn>
+    </div>
+    <div v-else-if="currentUserId">
+      <v-btn
+        class="white--text font-weight-bold"
+        color="#009eff"
+        @click="participate">
+        参加
+      </v-btn>
+    </div>
     <div class="talks-list">
       <talk-item
         v-for="talk in talks"
@@ -91,111 +72,54 @@
       return {
         event: [],
         talks: [],
+        participants: [],
+        isParticipated: false,
         author: {},
         currentUserId: '',
-        participants: [],
         participated: false,
         isAuthor: false,
       }
     },
     created() {
-      // console.dir(this.event);
+      console.log("created");
       let self = this;
-      // console.log('Event Page');
-      firebase.auth().onAuthStateChanged(user => {
+      firebase.auth().onAuthStateChanged(async(user) => {
         if (user != null) {
-          self.currentUserId = user.uid;
+          this.$root.$set(self, 'currentUserId', user.uid);
         }
       });
-      // let eventRef = db.collection('events').doc(this.$route.params['id']);
-      // eventRef
-      //   .get()
-      //   .then(event => {
-      //     if (event.exists) {
-      //       db.collection('talks').where('eventRef', '==', eventRef).get().then(talks => {
-      //         talks.forEach(async(talk) => {
-      //           // console.log(talk.id);
-      //           // console.log(talk.data());
-      //           let talkUser = await talk.data().userRef.get();
-      //           self.talks.push(
-      //             {
-      //               id: talk.id,
-      //               data: talk.data(),
-      //               talkUser: {
-      //                 id: talkUser.id,
-      //                 data: talkUser.data()
-      //               }
-      //             }
-      //           );
-      //         });
-      //       });
-      //       console.log('Successfully fetched event data');
-      //       // console.log(JSON.stringify(event.data()));
-      //       self.event = {
-      //         id: event.id,
-      //         data: event.data()
-      //       }
-      //       if (event.data().author == self.currentUserId) {
-      //         self.isAuthor = true;
-      //       }
-      //       if (event.data().participants.length) {
-      //         event.data().participants.forEach( async(userRef) => {
-      //           let user = await userRef.get();//参照型からデータの取得は非同期
-      //           self.participants.push({
-      //             id: user.id,
-      //             data: user.data()
-      //           });
-      //           if (user.id == self.currentUserId) {
-      //             self.participated = true;
-      //           }
-      //         });
-      //       }
-      //
-      //       db.collection('users')
-      //         .doc(event.data().author)
-      //         .get()
-      //         .then(author => {
-      //           if (author != null) {
-      //             self.author = {
-      //               id: author.id,
-      //               data: author.data()
-      //             }
-      //           }
-      //         })
-      //         .catch(err => {
-      //           console.error('Error fetching author data: ', err);
-      //         });
-      //
-      //     } else {
-      //       console.error('Error fetching event data');
-      //       self.event = {};
-      //     }
-      //   })
-      //   .catch(err => {
-      //     console.error('Error fetching event data: ', err);
-      //   });
-
+    },
+    watch: {
+      async event() {
+        let currentUserRef = await db.collection('users').doc(this.currentUserId)
+        let currentEventRef = await db.collection('events').doc(this.$route.params['id'])
+        let participantRef = await db.collection('participants')
+          .where('userRef', '==', currentUserRef)
+          .where('eventRef', '==', currentEventRef)
+          .get();
+        if (!participantRef.empty) {
+          this.isParticipated = true;
+        }
+      }
     },
     firestore(){
+      console.log("firestore")
       return {
         event: db.collection('events').doc(this.$route.params['id']),
         talks: db.collection('talks')
-          .where('eventRef', '==', db.collection('events').doc(this.$route.params['id']))
-
+          .where('eventRef', '==', db.collection('events').doc(this.$route.params['id'])),
+        participants: db.collection('participants')
+          .where('eventRef', '==', db.collection('events').doc(this.$route.params['id'])),
       }
     },
     methods: {
-      //   goUserPage() {
-      //     console.log('goUserPage');
-      //     this.$router.push({ name : 'user', params: { uid: this.author.id}});
-      //   },
       async deleteEvent() {
         var res = confirm('ほんとにイベントを取りやめますか？？？？？');
         if (res) {
           console.log('deleteEvent');
           let eventRef = await db.collection('events').doc(this.event.id); //参加イベントの参照オブジェクト
 
-          await this.participants.forEach( user => {
+          await this.participants.forEach(user => {
             db.collection('users')
               .doc(user.id)
               .update({
@@ -206,60 +130,41 @@
             .doc(this.$route.params['id'])
             .delete()
             .then(() => {
-              this.$router.push({ name : 'events'});
+              this.$router.push({name: 'events'});
             })
             .catch(err => {
               console.error('Error deleting event data: ', err);
             });
         }
       },
-      //   async participate() {
-      //     try {
-      //       let self = this;
-      //       let userRef = await db.collection('users').doc(self.currentUserId); //ログインユーザーの参照オブジェクト
-      //       let eventRef = await db.collection('events').doc(self.event.id); //参加イベントの参照オブジェクト
-      //       await userRef.update({
-      //         joinEvents: firebase.firestore.FieldValue.arrayUnion(eventRef)
-      //       });
-      //       await eventRef.update({
-      //         participants: firebase.firestore.FieldValue.arrayUnion(userRef)
-      //       });
-      //       console.log('participants registered');
-      //       this.$router.go(this.$router.currentRoute);
-      //     } catch (err) {
-      //       console.log(err);
-      //     }
-      //   },
-      //   async cancelParticipate() {
-      //     var res = confirm('ほんとに取りやめますか？？？？？');
-      //     if (res) {
-      //       try {
-      //         let self = this;
-      //         let userRef = await db.collection('users').doc(self.currentUserId); //ログインユーザーの参照オブジェクト
-      //         let eventRef = await db.collection('events').doc(self.event.id); //参加イベントの参照オブジェクト
-      //         await userRef.update({
-      //           joinEvents: firebase.firestore.FieldValue.arrayRemove(eventRef)
-      //         });
-      //         await eventRef.update({
-      //           participants: firebase.firestore.FieldValue.arrayRemove(userRef)
-      //         });
-      //         let talkRefs = await db.collection('talks').where('eventRef', '==', eventRef).where('userRef', '==', userRef);
-      //         await talkRefs
-      //           .get()
-      //           .then(talks => {
-      //             talks.forEach(talk => {
-      //               talk.ref.delete().then(() => {
-      //                 console.log('delete!');
-      //               });
-      //             });
-      //           });
-      //         alert('次はないですよ');
-      //         this.$router.go(this.$router.currentRoute);
-      //       } catch (err) {
-      //         console.log(err);
-      //       }
-      //     }
-      //   },
+      async participate() {
+        let self = this;
+        let userRef = await db.collection('users').doc(self.currentUserId); //ログインユーザーの参照オブジェクト
+        let eventRef = await db.collection('events').doc(self.event.id); //参加イベントの参照オブジェクト
+        db.collection('participants').add({
+          userRef: userRef,
+          eventRef: eventRef
+        });
+        this.isParticipated = true;
+      },
+      async cancelParticipate() {
+        var res = confirm('ほんとに取りやめますか？？？？？');
+        if (res) {
+          this.isParticipated = false;
+          let currentUserRef = await db.collection('users').doc(this.currentUserId)
+          let currentEventRef = await db.collection('events').doc(this.$route.params['id'])
+          db.collection('participants')
+            .where('userRef', '==', currentUserRef)
+            .where('eventRef', '==', currentEventRef)
+            .get()
+            .then(participants => {
+              participants.forEach(participant =>{
+                participant.ref.delete();
+              })
+            });
+        }
+        alert('次はないですよ');
+      },
       //日付から文字列に変換する関数
       getStringFromDate(date) {
         var year_str = date.getFullYear();

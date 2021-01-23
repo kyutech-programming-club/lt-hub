a1d4600d63e7d538da2506333720536addac8e44
syu-kwsk <wataru1165kwsk@gmail.com>
2021-01-23 17:52:42 +0900

    feat: #120 Add user interface and user converter
    Because change user obj from firestore to user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,52 @@
-interface User {
+import firebase from "firebase";
+
+export interface User {
   id: string;
-  data: firebase.firestore.DocumentData | undefined;
+  name: string;
+  belong: string;
+  photoUrl?: string;
+  twitterUrl?: string;
+  githubUrl?: string;
+  createdTime: Date;
+  updatedTime: Date;
+}
+export interface UserDoc {
+  name: string;
+  belong: string;
+  photoUrl: string;
+  twitterUrl?: string;
+  githubUrl?: string;
+  createdTime: firebase.firestore.Timestamp;
+  updatedTime: firebase.firestore.Timestamp;
 }
 
-export default User;
+export const userConverter: firebase.firestore.FirestoreDataConverter<User> = {
+  toFirestore(user: User): UserDoc {
+    return {
+      name: user.name,
+      belong: user.belong,
+      photoUrl: user.photoUrl,
+      twitterUrl: user.twitterUrl,
+      githubUrl: user.githubUrl,
+      createdTime: firebase.firestore.Timestamp.fromDate(user.createdTime),
+      updatedTime: firebase.firestore.Timestamp.fromDate(user.updatedTime),
+    } as UserDoc;
+  },
+
+  fromFirestore(
+    snapshot: firebase.firestore.QueryDocumentSnapshot,
+    options: firebase.firestore.SnapshotOptions
+  ): User {
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      name: data.name,
+      belong: data.belong,
+      photoUrl: data.photoUrl,
+      twitterUrl: data.twitterUrl,
+      githubUrl: data.githubUrl,
+      createdTime: data.createdTime.toDate(),
+      updatedTime: data.updatedTime.toDate(),
+    } as User;
+  },
+};

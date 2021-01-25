91cfd24a30fbed772f8180b41dc85b479606f788
yattinda <yattinda@gmail.com>
2021-01-25 09:58:26 +0900

    test #121 Count managed by vuex
    to test vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
     <h1>Welcome to LT-Hub!</h1>
+    <p>{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+    </p>
     <v-img alt="Vue logo" src="@/assets/logo.png" />
   </div>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component
+export default class Home extends Vue {
+  get count(): number {
+    return this.$store.state.count;
+  }
+  increment(): void {
+    this.$store.commit("increment");
+  }
+}
+</script>
 <style scoped></style>

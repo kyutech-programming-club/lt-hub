8276db4b6ebb5ce4f57340fcfd21d7570ce709ee
syu-kwsk <wataru1165kwsk@gmail.com>
2021-02-14 15:53:56 +0900

    feat: #126 Show photo url
    to show login user profile

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="logout-button">
+    <v-avatar>
+      <img :src="this.user.photoUrl" />
+    </v-avatar>
     <v-btn
       class="ma-2"
       style="text-transform: none"
@@ -17,11 +20,19 @@
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
+import { getUserData } from "@/repository/userRepository";
+import { User } from "@/types/user";
 import router from "@/router";
 import firebase from "firebase";
 
 @Component
 export default class LogoutButton extends Vue {
+  user?: User = {} as User;
+
+  async created(): Promise<void> {
+    this.user = await getUserData();
+  }
+
   doLogout(): void {
     firebase.auth().signOut();
     if (router.currentRoute.path !== "/") {

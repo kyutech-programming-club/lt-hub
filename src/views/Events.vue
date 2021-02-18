063971e1cbc3b073949ea17f3535a6022ba4509e
syu-kwsk <wataru1165kwsk@gmail.com>
2021-02-18 19:36:30 +0900

    feat: #131 Add events page
    to show event list

---
+++ src/views/Events.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="events">
+    <h1>Events</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import Component from "vue-class-component";
+import { User } from "@/types/user";
+
+const EventsProps = Vue.extend({
+  props: {
+    currentUser: Object as PropType<User>,
+  },
+});
+
+@Component({})
+export default class Events extends EventsProps {
+  created(): void {
+    console.dir(this.currentUser);
+  }
+}
+</script>
+<style scoped></style>
